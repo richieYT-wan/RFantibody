41517fb377ece7845e75cf61ab247056041d353f
richieYT-wan <richieytwan@yahoo.fr>
2026-02-17 15:31:50 +0000

    Initial Snakemake config; WIP

--- multiple_concurrent_jobs.sh
+++ multiple_concurrent_jobs.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail
+#!/bin/bash
+
+# Helper script to run multiple jobs in parallel on VertexAI when using multiple GPUs
+# (assumes in the job definition that different --cuda-device have been set
+# Ex: jobs that match the pattern "job_type_0" all have --cuda-device 0 in the run scripts
+
+show_help() {
+  echo "Usage: myscript.sh [PATTERN] [N_JOBS] [JOBDIR]"
+  echo ""
+  echo "Arguments:"
+  echo "  PATTERN    e.g. quiver_7"
+  echo "  N_JOBS     e.g. 5"
+  echo "  JOBDIR     path/to/jobs/"
+  echo ""
+  echo "Options:"
+  echo "  -h         Show this help message"
+}
+
+if [[ "$1" == "-h" || "$1" == "--help" || $# -eq 0 ]]; then
+  show_help
+  exit 0
+fi
 
 pattern="${1:?pattern required, e.g. quiver_7}"
 NJOBS="${2:?n_jobs required, e.g. 5}"
